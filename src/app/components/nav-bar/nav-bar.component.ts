64f3a7f57116c52a1f941a18ad06c37da621ab37
Naveen Manthri <naveenmanthri16@gmail.com>
2025-09-02 19:41:23 +0530

    Formatted the html code

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import { Subscription, filter } from "rxjs";
 import { AppConstants } from "src/app/constants/app-constants";

da353362e36b1a06f446e5cca6c2c2f8b70b8f81
Naveen Manthri <naveenmanthri16@gmail.com>
2025-09-03 17:35:01 +0530

    Updated the images and content for the Club Activites and Industrial Visit

--- src/app/pages/academics/club-activities/club-activities.component.ts
+++ src/app/pages/academics/club-activities/club-activities.component.ts
@@ -9,6 +9,15 @@ export class ClubActivitiesComponent implements OnInit {
 
   constructor() { }
 
+  images = [
+    { url: 'assets/club-activities/Adventure.jpeg', alt: 'Club Activity 1' },
+    { url: 'assets/club-activities/Competitive.jpeg', alt: 'Club Activity 2' },
+    { url: 'assets/club-activities/Finance.png', alt: 'Club Activity 3' },
+    { url: 'assets/club-activities/GSB.png', alt: 'Club Activity 4' },
+    { url: 'assets/club-activities/HR.jpeg', alt: 'Club Activity 5' },
+    { url: 'assets/club-activities/Spiritual.jpeg', alt: 'Club Activity 6' },
+  ]
+
   ngOnInit(): void {
   }
 
--- src/app/pages/academics/industrial-visit/industrial-visit.component.ts
+++ src/app/pages/academics/industrial-visit/industrial-visit.component.ts
@@ -9,6 +9,18 @@ export class IndustrialVisitComponent implements OnInit {
 
   constructor() { }
 
+  openLink(url: string) {
+    window.open(url, '_blank');
+  }
+
+  industries = [
+    { url: 'assets/industrial-visits/diary-day.jpeg', alt: 'Industry 1', link: 'https://dairyday.com/', linkText: 'DAIRY DAY' },
+    { url: 'assets/industrial-visits/akshayakalpa.jpeg', alt: 'Industry 2', link: 'https://akshayakalpa.org/', linkText: 'AKSHAYAKALPA' },
+    { url: 'assets/industrial-visits/eagles-unbound.JPG', alt: 'Industry 3', link: 'https://eaglesunbound.com/', linkText: 'EAGLES UNBOUND' },
+    { url: 'assets/industrial-visits/ksdl.jpeg', alt: 'Industry 4', link: 'https://mysoresandal.karnataka.gov.in/', linkText: 'KSDL' },
+    { url: 'assets/industrial-visits/festo.jpeg', alt: 'Industry 5', link: 'https://festo.com/', linkText: 'FESTO' },
+  ]
+
   ngOnInit(): void {
   }
 
